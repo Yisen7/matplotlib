3003da22aaae70ff55e83c0df3dc3c3e8ca070f3
root <13212152757@163.com>
2019-04-03 15:26:47 +0800

    population sorted by 3 Groups(<10m,10m-1bn,>1bn)

--- src/downloadData/world_population.py
+++ src/downloadData/world_population.py
@@ -13,11 +13,25 @@ for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(country + ":" + str(population))
+        # print(country + ":" + str(population))
         code = get_country_code(country)
         if code:
             cc_populations[code] = population
+
+# 根据国家人口数量将所有的国家分为3组
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:cc_pops_3[cc] = pop
+# 查看每个分组内的国家的数量
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
 wm = pygal_maps_world.maps.World()
 wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
-wm.render_to_file('world_population4.svg')
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+wm.render_to_file('world_population.svg')
